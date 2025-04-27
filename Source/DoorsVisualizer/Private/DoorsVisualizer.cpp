be4bf2eb90c5eeabc4ece3d74f131936cb63f80b
Jared Taylor <jaredtaylor.99@gmail.com>
2025-04-28 11:34:47 +1200

    Draw fancy door sprites

--- Source/DoorsVisualizer/Private/DoorsVisualizer.cpp
+++ Source/DoorsVisualizer/Private/DoorsVisualizer.cpp
@@ -11,6 +11,16 @@
 
 void FDoorsVisualizerModule::StartupModule()
 {
+	// Load icons for door state sprites
+	FDoorVisualizer::ClosedOutwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Closed_Outward.T_Door_Sprite_Closed_Outward"));
+	FDoorVisualizer::ClosedInwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Closed_Inward.T_Door_Sprite_Closed_Inward"));
+	FDoorVisualizer::OpenOutwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Open_Outward.T_Door_Sprite_Open_Outward"));
+	FDoorVisualizer::OpenInwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Open_Inward.T_Door_Sprite_Open_Inward"));
+	FDoorVisualizer::OpeningOutwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Opening_Outward.T_Door_Sprite_Opening_Outward"));
+	FDoorVisualizer::OpeningInwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Opening_Inward.T_Door_Sprite_Opening_Inward"));
+	FDoorVisualizer::ClosingOutwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Closing_Outward.T_Door_Sprite_Closing_Outward"));
+	FDoorVisualizer::ClosingInwardSprite = LoadObject<UTexture2D>(nullptr, TEXT("/Doors/Editor/T_Door_Sprite_Closing_Inward.T_Door_Sprite_Closing_Inward"));
+	
 	GUnrealEd->RegisterComponentVisualizer(UDoorEditorVisualizer::StaticClass()->GetFName(), MakeShareable(new FDoorVisualizer()));
 }
 
--- Source/DoorsVisualizer/Public/DoorVisualizer.h
+++ Source/DoorsVisualizer/Public/DoorVisualizer.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "ComponentVisualizer.h"
 
+class ADoor;
 /**
  * Draws editor visualizers for doors
  * This visualizes the door's capabilities
@@ -12,6 +13,18 @@
 class DOORSVISUALIZER_API FDoorVisualizer : public FComponentVisualizer
 {
 public:
+	static UTexture2D* ClosedInwardSprite;
+	static UTexture2D* ClosedOutwardSprite;
+	static UTexture2D* OpenInwardSprite;
+	static UTexture2D* OpenOutwardSprite;
+	static UTexture2D* OpeningInwardSprite;
+	static UTexture2D* OpeningOutwardSprite;
+	static UTexture2D* ClosingInwardSprite;
+	static UTexture2D* ClosingOutwardSprite;
+	
+public:
+	virtual UTexture2D* GetDoorSprite(const ADoor* Door) const;
+	
 	virtual void DrawVisualization(const UActorComponent* Component, const FSceneView* View,
 		FPrimitiveDrawInterface* PDI) override;
 };
