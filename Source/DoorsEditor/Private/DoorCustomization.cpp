dfffebcff6d5deed3189bc9989df75bf5f0d2d68
Jared Taylor <jaredtaylor.99@gmail.com>
2025-04-30 17:00:48 +1200

    Add additional door categories as important

--- Source/DoorsEditor/Private/DoorCustomization.cpp
+++ Source/DoorsEditor/Private/DoorCustomization.cpp
@@ -14,4 +14,14 @@ TSharedRef<IDetailCustomization> FDoorCustomization::MakeInstance()
 void FDoorCustomization::CustomizeDetails(IDetailLayoutBuilder& DetailBuilder)
 {
 	DetailBuilder.EditCategory(TEXT("Door"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("DoorChange"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("Door Change"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("DoorProperties"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("Door Properties"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("DoorAlpha"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("Door Alpha"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("DoorTime"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("Door Time"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("DoorPreview"), FText::GetEmpty(), ECategoryPriority::Important);
+	DetailBuilder.EditCategory(TEXT("Door Preview"), FText::GetEmpty(), ECategoryPriority::Important);
 }
