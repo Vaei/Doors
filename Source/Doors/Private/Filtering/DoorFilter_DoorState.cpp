11bb7a96e9541c9b338b334be5c7e6aebcba99b4
Jared Taylor <jaredtaylor.99@gmail.com>
2025-05-02 17:05:41 +1200

    Add filter by door state (untested)

---
+++ Source/Doors/Private/Filtering/DoorFilter_DoorState.cpp
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Jared Taylor
+
+
+#include "Filtering/DoorFilter_DoorState.h"
+
+#include "Door.h"
+#include "GraspComponent.h"
+#include "GraspStatics.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(DoorFilter_DoorState)
+
+
+UDoorFilter_DoorState::UDoorFilter_DoorState(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{}
+
+bool UDoorFilter_DoorState::ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle,
+	const FTargetingDefaultResultData& TargetData) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(DoorFilter_DoorState::ShouldFilterTarget);
+
+	// Find the source actor
+	const FTargetingSourceContext* SourceContext = FTargetingSourceContext::Find(TargetingHandle);
+	if (!SourceContext || !IsValid(SourceContext->SourceActor))
+	{
+		return true;
+	}
+	
+	const UPrimitiveComponent* TargetComponent = TargetData.HitResult.GetComponent();
+	
+	const ADoor* Door = TargetComponent ? Cast<ADoor>(TargetComponent->GetOwner()) : nullptr;
+	if (!Door)
+	{
+		return bFilterIfNotDoor;
+	}
+
+	if (IsValueFiltered(DoorStateFilterType, Door->GetDoorState(), DoorStates))
+	{
+		return true;
+	}
+
+	if (IsValueFiltered(DoorDirectionFilterType, Door->GetDoorDirection(), DoorDirections))
+	{
+		return true;
+	}
+
+	if (IsValueFiltered(DoorAccessFilterType, Door->GetDoorAccess(), DoorAccesses))
+	{
+		return true;
+	}
+
+	if (IsValueFiltered(DoorOpenDirectionFilterType, Door->GetDoorOpenDirection(), DoorOpenDirections))
+	{
+		return true;
+	}
+
+	return false;
+}
---
+++ Source/Doors/Public/Filtering/DoorFilter_DoorState.h
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Jared Taylor
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DoorTypes.h"
+#include "Tasks/TargetingFilterTask_BasicFilterTemplate.h"
+#include "DoorFilter_DoorState.generated.h"
+
+UENUM(BlueprintType)
+enum class EDoorFilterType : uint8
+{
+	Ignore,
+	Blacklist,
+	Whitelist
+};
+
+/**
+ * Filter targets based on their being a door and in a specific state
+ */
+UCLASS(Blueprintable, DisplayName="Door Filter (Door State)")
+class DOORS_API UDoorFilter_DoorState : public UTargetingFilterTask_BasicFilterTemplate
+{
+	GENERATED_BODY()
+
+public:
+	/** If its not a door at all, will be filtered out if true */
+	UPROPERTY(EditAnywhere, Category="Door Filter")
+	bool bFilterIfNotDoor = false;
+	
+	/** Whether to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter")
+	EDoorFilterType DoorStateFilterType = EDoorFilterType::Ignore;
+
+	/** Whether to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter")
+	EDoorFilterType DoorDirectionFilterType = EDoorFilterType::Ignore;
+	
+	/** Whether to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter")
+	EDoorFilterType DoorAccessFilterType = EDoorFilterType::Ignore;
+		
+	/** Whether to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter")
+	EDoorFilterType DoorOpenDirectionFilterType = EDoorFilterType::Ignore;
+	
+	/** Door states to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter", meta=(EditCondition="DoorStateFilterType!=EDoorFilterType::Ignore", EditConditionHides))
+	TArray<EDoorState> DoorStates = { EDoorState::Closed, EDoorState::Closing, EDoorState::Open, EDoorState::Opening };
+
+	/** Door directions to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter", meta=(EditCondition="DoorDirectionFilterType!=EDoorFilterType::Ignore", EditConditionHides))
+	TArray<EDoorDirection> DoorDirections = { EDoorDirection::Inward, EDoorDirection::Outward };
+
+	/** Door access to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter", meta=(EditCondition="DoorAccessFilterType!=EDoorFilterType::Ignore", EditConditionHides))
+	TArray<EDoorAccess> DoorAccesses = { EDoorAccess::Bidirectional, EDoorAccess::Behind, EDoorAccess::Front };
+
+	/** Door access to filter in or out */
+	UPROPERTY(EditAnywhere, Category="Door Filter", meta=(EditCondition="DoorOpenDirectionFilterType!=EDoorFilterType::Ignore", EditConditionHides))
+	TArray<EDoorOpenDirection> DoorOpenDirections = { EDoorOpenDirection::Bidirectional, EDoorOpenDirection::Inward, EDoorOpenDirection::Outward, EDoorOpenDirection::Locked };
+	
+public:
+	UDoorFilter_DoorState(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	/** Called against every target data to determine if the target should be filtered out */
+	virtual bool ShouldFilterTarget(const FTargetingRequestHandle& TargetingHandle, const FTargetingDefaultResultData& TargetData) const override;
+
+protected:
+	// Helper for filtering values based on filter type
+	template<typename T>
+	bool IsValueFiltered(EDoorFilterType FilterType, T Value, const TArray<T>& FilterList) const;
+};
+
+template <typename T>
+bool UDoorFilter_DoorState::IsValueFiltered(EDoorFilterType FilterType, T Value, const TArray<T>& FilterList) const
+{
+	switch (FilterType)
+	{
+	case EDoorFilterType::Whitelist:
+		return !FilterList.Contains(Value);
+	case EDoorFilterType::Blacklist:
+		return FilterList.Contains(Value);
+	case EDoorFilterType::Ignore:
+	default:
+		return false;
+	}
+}
